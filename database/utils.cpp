1e90061e9990b9279ef7f7d2c6de2ff4c40ba2cf
James Zhang <jzhang72@terpmail.umd.edu>
2023-07-29 16:38:49 -0600

    fixed get_data function

--- database/utils.cpp
+++ database/utils.cpp
@@ -11,34 +11,28 @@ std::vector<std::vector<ld>> execute_query(std::string query) {
     try {
         // Create a MySQL driver instance.
         sql::mysql::MySQL_Driver *driver = sql::mysql::get_mysql_driver_instance();
-
         // Connect to the MySQL server.
         sql::Connection *con = driver->connect(server, username, password);
-
         // Connect to the specified database
         con->setSchema(database);
-
         // Prepare the statement and execute it.
         sql::PreparedStatement *prep_stmt = con->prepareStatement(query);
         sql::ResultSet *res = prep_stmt->executeQuery();
-
         // Get the result set metadata
         sql::ResultSetMetaData *meta = res->getMetaData();
-
         // Get the number of columns.
         int numColumns = meta->getColumnCount();
-
         // Get all of the values in the rows.
         while (res->next()) {
-            data.push_back(std::vector<ld>(numColumns));
-            for (int i = 1; i <= numColumns; i++) {
-                data[data.size() - 1][i] = res->getDouble(i);
+            data.push_back(std::vector<ld>(numColumns - 1));
+            for (int i = 2; i <= numColumns; i++) {
+                data[data.size() - 1][i - 2] = res->getDouble(i);
             }
         }
-
-        delete res;
-        delete prep_stmt;
+        delete driver;
         delete con;
+        delete prep_stmt;
+        delete res;
         return data;
 
     } catch (sql::SQLException &e) {
@@ -59,45 +53,37 @@ std::unordered_map<int, std::string> get_universe(std::string factor,
     try {
         // Create a MySQL driver instance.
         sql::mysql::MySQL_Driver *driver = sql::mysql::get_mysql_driver_instance();
-
         // Connect to the MySQL server.
         sql::Connection *con = driver->connect(server, username, password);
-
         // Connect to the specified database
         con->setSchema(database);
-
         // Prepare the statement and execute it.
         std::string query = "SHOW COLUMNS FROM " + exchange + "-" + factor;
         sql::PreparedStatement *prep_stmt = con->prepareStatement(query);
         sql::ResultSet *res = prep_stmt->executeQuery();
-
         // Get the result set metadata
         sql::ResultSetMetaData *meta = res->getMetaData();
-
         // Get the column names.
         int numColumns = meta->getColumnCount();
         for (int i = 1; i <= numColumns; i++) {
             universe[i] = meta->getColumnName(i);
         }
-
         delete res;
         delete prep_stmt;
         delete con;
-
+        return universe;
     } catch (sql::SQLException &e) {
         std::cerr << "MySQL Error: " << e.what() << std::endl;
         return {};
     }
 }
 
-
 std::vector<std::vector<ld>> get_data(std::string start_date,
                                       std::string end_date, 
                                       std::string factor, 
                                       std::string exchange) {
-    std::string query = "SELECT * FROM " + exchange + "-" + factor;
-    query += " WHERE timestamp BETWEEN " + start_date + " AND " + end_date;
+    std::string query = "SELECT * FROM `" + exchange + "-" + factor;
+    query += "` WHERE `timestamp` BETWEEN '" + start_date + "' AND '" + end_date + "'";
     auto result = execute_query(query);
     return result;
 }
-
